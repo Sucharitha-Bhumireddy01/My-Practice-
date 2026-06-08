e84d724e12a66e452c61354d96a5ee6a64989f22
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 12:49:38 +0530

    Add files via upload

---
+++ AreaofCircle/Program.cs
@@ -0,0 +1,11 @@
+﻿class Circle
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Plese Enter the Radius ");
+        double Radius=System.Convert.ToDouble(System.Console.ReadLine());
+        double Area = System.Math.PI*Radius* Radius;
+        System.Console.WriteLine("Area of the Circle: "+Area);
+        System.Console.ReadKey();
+    }
+}
