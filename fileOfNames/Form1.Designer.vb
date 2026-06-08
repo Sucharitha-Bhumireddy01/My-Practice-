a1fc2f558ce34cdac8982d220db5db8b2a4a1155
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 17:19:57 +0530

    Add files via upload

---
+++ fileOfNames/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFileofNames
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(66, 150)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(438, 70)
+        Me.btnInsert.TabIndex = 0
+        Me.btnInsert.Text = "Insert Name"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(62, 93)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(118, 20)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Name to Insert:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(186, 93)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(318, 26)
+        Me.txtName.TabIndex = 2
+        '
+        'frmFileofNames
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 294)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Name = "frmFileofNames"
+        Me.Text = "File of Names"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnInsert As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+End Class
