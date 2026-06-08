41d43c447f0f242c5c07d8ccbc0afad7f4040435
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 13:18:48 +0530

    Add files via upload

---
+++ prime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Number to check it is prime Number ");
+            int n= Int32.Parse(Console.ReadLine());
+            int count = 0;
+            
+            for (int i=1;i<=n;i++) {
+                if(n%i==0) 
+                {
+                    count++;
+                } 
+            }
+            if (count == 2) {
+                Console.WriteLine("The Entered Number is Prime Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not Prime Number");
+                Console.Read();
+            }
+        }
+    }
+}
