43b19d19e034d2b82d9eaa1efa6260287d44e11a
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 18:59:25 +0530

    Add files via upload

---
+++ unitPrice/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUnitPrice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnUnitPrice = New System.Windows.Forms.Button()
+        Me.txtPriceOfItem = New System.Windows.Forms.TextBox()
+        Me.txtPounds = New System.Windows.Forms.TextBox()
+        Me.txtOunces = New System.Windows.Forms.TextBox()
+        Me.txtPricePerOunce = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(153, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Price of item"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(290, 121)
+        Me.Label2.MaximumSize = New System.Drawing.Size(100, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 33)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Weight"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(97, 179)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Pounds"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(360, 179)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Ounces"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(153, 351)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(123, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Price Per Ounce"
+        '
+        'btnUnitPrice
+        '
+        Me.btnUnitPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUnitPrice.Location = New System.Drawing.Point(146, 254)
+        Me.btnUnitPrice.Name = "btnUnitPrice"
+        Me.btnUnitPrice.Size = New System.Drawing.Size(341, 75)
+        Me.btnUnitPrice.TabIndex = 5
+        Me.btnUnitPrice.Text = "Determine Unit Price"
+        Me.btnUnitPrice.UseVisualStyleBackColor = True
+        '
+        'txtPriceOfItem
+        '
+        Me.txtPriceOfItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPriceOfItem.Location = New System.Drawing.Point(290, 59)
+        Me.txtPriceOfItem.Name = "txtPriceOfItem"
+        Me.txtPriceOfItem.Size = New System.Drawing.Size(100, 26)
+        Me.txtPriceOfItem.TabIndex = 6
+        '
+        'txtPounds
+        '
+        Me.txtPounds.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPounds.Location = New System.Drawing.Point(176, 176)
+        Me.txtPounds.Name = "txtPounds"
+        Me.txtPounds.Size = New System.Drawing.Size(100, 26)
+        Me.txtPounds.TabIndex = 7
+        '
+        'txtOunces
+        '
+        Me.txtOunces.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOunces.Location = New System.Drawing.Point(430, 173)
+        Me.txtOunces.Name = "txtOunces"
+        Me.txtOunces.Size = New System.Drawing.Size(100, 26)
+        Me.txtOunces.TabIndex = 8
+        '
+        'txtPricePerOunce
+        '
+        Me.txtPricePerOunce.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPricePerOunce.Location = New System.Drawing.Point(318, 351)
+        Me.txtPricePerOunce.Name = "txtPricePerOunce"
+        Me.txtPricePerOunce.ReadOnly = True
+        Me.txtPricePerOunce.Size = New System.Drawing.Size(169, 26)
+        Me.txtPricePerOunce.TabIndex = 9
+        '
+        'frmUnitPrice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtPricePerOunce)
+        Me.Controls.Add(Me.txtOunces)
+        Me.Controls.Add(Me.txtPounds)
+        Me.Controls.Add(Me.txtPriceOfItem)
+        Me.Controls.Add(Me.btnUnitPrice)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmUnitPrice"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Unit Price"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnUnitPrice As Button
+    Friend WithEvents txtPriceOfItem As TextBox
+    Friend WithEvents txtPounds As TextBox
+    Friend WithEvents txtOunces As TextBox
+    Friend WithEvents txtPricePerOunce As TextBox
+End Class
---
+++ unitPrice/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmUnitPrice
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub btnUnitPrice_Click(sender As Object, e As EventArgs) Handles btnUnitPrice.Click
+        Dim price, pricePerOunce As Double
+        Dim pounds, ounces As Integer
+
+        price = CDbl(txtPriceOfItem.Text)
+        pounds = CInt(txtPounds.Text)
+        ounces = CInt(txtOunces.Text)
+
+        Dim totalOunces = ounces + (pounds * 16)
+
+        pricePerOunce = price / totalOunces
+        txtPricePerOunce.Text = pricePerOunce.ToString("C2")
+
+
+    End Sub
+End Class
