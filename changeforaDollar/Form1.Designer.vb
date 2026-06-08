17470b59edecfb1655ba359ec89bfc3e74a68912
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 17:16:00 +0530

    Add files via upload

---
+++ changeforaDollar/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.txt25 = New System.Windows.Forms.TextBox()
+        Me.txt10 = New System.Windows.Forms.TextBox()
+        Me.txt5 = New System.Windows.Forms.TextBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(312, 355)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(122, 70)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate Change"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(312, 50)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(122, 26)
+        Me.txtAmount.TabIndex = 1
+        '
+        'txt25
+        '
+        Me.txt25.Location = New System.Drawing.Point(312, 151)
+        Me.txt25.Name = "txt25"
+        Me.txt25.ReadOnly = True
+        Me.txt25.Size = New System.Drawing.Size(122, 26)
+        Me.txt25.TabIndex = 2
+        '
+        'txt10
+        '
+        Me.txt10.Location = New System.Drawing.Point(312, 197)
+        Me.txt10.Name = "txt10"
+        Me.txt10.ReadOnly = True
+        Me.txt10.Size = New System.Drawing.Size(122, 26)
+        Me.txt10.TabIndex = 3
+        '
+        'txt5
+        '
+        Me.txt5.Location = New System.Drawing.Point(312, 241)
+        Me.txt5.Name = "txt5"
+        Me.txt5.ReadOnly = True
+        Me.txt5.Size = New System.Drawing.Size(122, 26)
+        Me.txt5.TabIndex = 4
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(312, 293)
+        Me.txt1.Name = "txt1"
+        Me.txt1.ReadOnly = True
+        Me.txt1.Size = New System.Drawing.Size(122, 26)
+        Me.txt1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(166, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Enter Amount"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 156)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "25c"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(230, 203)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "10c"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(239, 247)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "5c"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(239, 293)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(26, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "1c"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.txt5)
+        Me.Controls.Add(Me.txt10)
+        Me.Controls.Add(Me.txt25)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Change for A Dollar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents txt25 As TextBox
+    Friend WithEvents txt10 As TextBox
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ changeforaDollar/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim amount, cents25, cents10, cents5, cents1 As Double
+
+        amount = CDbl(txtAmount.Text)
+
+        cents25 = Int(amount / 25)
+        txt25.Text = cents25
+
+        amount = amount - (25 * cents25)
+
+        cents10 = Int(amount / 10)
+        txt10.Text = cents10
+
+        amount = amount - (10 * cents10)
+
+        cents5 = Int(amount / 5)
+        txt5.Text = cents5
+
+        amount = amount - (5 * cents5)
+
+        cents1 = amount
+        txt1.Text = cents1
+    End Sub
+End Class
