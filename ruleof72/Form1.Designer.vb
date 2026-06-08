b4ae33c9337325d7965eb051de4fef2f60966459
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 18:33:01 +0530

    Add files via upload

---
+++ ruleof72/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRule72
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEvaluate = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnEvaluate
+        '
+        Me.btnEvaluate.Location = New System.Drawing.Point(39, 33)
+        Me.btnEvaluate.Name = "btnEvaluate"
+        Me.btnEvaluate.Size = New System.Drawing.Size(341, 52)
+        Me.btnEvaluate.TabIndex = 0
+        Me.btnEvaluate.Text = "Evaluate Rule"
+        Me.btnEvaluate.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 20
+        Me.lstOutput.Location = New System.Drawing.Point(39, 124)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(341, 184)
+        Me.lstOutput.TabIndex = 1
+        '
+        'frmRule72
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnEvaluate)
+        Me.Name = "frmRule72"
+        Me.Text = "Rule 72"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnEvaluate As Button
+    Friend WithEvents lstOutput As ListBox
+End Class
