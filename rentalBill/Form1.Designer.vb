f3a6ac9d40b6102eecc5dee6f1fffa94ec22b101
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 18:03:11 +0530

    Add files via upload

---
+++ rentalBill/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmRentalBill
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtItem = New System.Windows.Forms.TextBox()
+        Me.txtDuration = New System.Windows.Forms.TextBox()
+        Me.btnDisplayRates = New System.Windows.Forms.Button()
+        Me.btnDisplayBill = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Equipment = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.HalfDay = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FullDay = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtItem
+        '
+        Me.txtItem.Location = New System.Drawing.Point(199, 181)
+        Me.txtItem.Name = "txtItem"
+        Me.txtItem.Size = New System.Drawing.Size(44, 26)
+        Me.txtItem.TabIndex = 2
+        '
+        'txtDuration
+        '
+        Me.txtDuration.Location = New System.Drawing.Point(199, 235)
+        Me.txtDuration.Name = "txtDuration"
+        Me.txtDuration.Size = New System.Drawing.Size(44, 26)
+        Me.txtDuration.TabIndex = 3
+        '
+        'btnDisplayRates
+        '
+        Me.btnDisplayRates.Location = New System.Drawing.Point(26, 64)
+        Me.btnDisplayRates.Name = "btnDisplayRates"
+        Me.btnDisplayRates.Size = New System.Drawing.Size(198, 59)
+        Me.btnDisplayRates.TabIndex = 4
+        Me.btnDisplayRates.Text = "Display Rental Rates"
+        Me.btnDisplayRates.UseVisualStyleBackColor = True
+        '
+        'btnDisplayBill
+        '
+        Me.btnDisplayBill.Location = New System.Drawing.Point(26, 296)
+        Me.btnDisplayBill.Name = "btnDisplayBill"
+        Me.btnDisplayBill.Size = New System.Drawing.Size(198, 57)
+        Me.btnDisplayBill.TabIndex = 5
+        Me.btnDisplayBill.Text = "Display Customer Bill"
+        Me.btnDisplayBill.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 184)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Select Item (1, 2, or 3)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 241)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(181, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Select Duration (H, or F)"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Equipment, Me.HalfDay, Me.FullDay})
+        Me.DataGridView1.Location = New System.Drawing.Point(249, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(515, 167)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Equipment
+        '
+        Me.Equipment.HeaderText = "Equipment"
+        Me.Equipment.MinimumWidth = 8
+        Me.Equipment.Name = "Equipment"
+        Me.Equipment.Width = 150
+        '
+        'HalfDay
+        '
+        Me.HalfDay.HeaderText = "Half Day"
+        Me.HalfDay.MinimumWidth = 8
+        Me.HalfDay.Name = "HalfDay"
+        Me.HalfDay.Width = 150
+        '
+        'FullDay
+        '
+        Me.FullDay.HeaderText = "FullDay"
+        Me.FullDay.MinimumWidth = 8
+        Me.FullDay.Name = "FullDay"
+        Me.FullDay.Width = 150
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column6})
+        Me.DataGridView2.Location = New System.Drawing.Point(270, 241)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidth = 62
+        Me.DataGridView2.RowTemplate.Height = 28
+        Me.DataGridView2.Size = New System.Drawing.Size(813, 179)
+        Me.DataGridView2.TabIndex = 9
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Equipment"
+        Me.Column1.MinimumWidth = 8
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 150
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Duration"
+        Me.Column2.MinimumWidth = 8
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 150
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Cost of Equipment"
+        Me.Column3.MinimumWidth = 8
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 150
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Deposit"
+        Me.Column4.MinimumWidth = 8
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 150
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Total Cost"
+        Me.Column6.MinimumWidth = 8
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 150
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(877, 50)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FrmRentalBill
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1090, 497)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDisplayBill)
+        Me.Controls.Add(Me.btnDisplayRates)
+        Me.Controls.Add(Me.txtDuration)
+        Me.Controls.Add(Me.txtItem)
+        Me.Name = "FrmRentalBill"
+        Me.Text = "Rental Bill"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtItem As TextBox
+    Friend WithEvents txtDuration As TextBox
+    Friend WithEvents btnDisplayRates As Button
+    Friend WithEvents btnDisplayBill As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Equipment As DataGridViewTextBoxColumn
+    Friend WithEvents HalfDay As DataGridViewTextBoxColumn
+    Friend WithEvents FullDay As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+End Class
---
+++ rentalBill/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class FrmRentalBill
+    Private Sub btnDisplayRates_Click(sender As Object, e As EventArgs) Handles btnDisplayRates.Click
+        'lstRentalRates.Items.Add("Price of Equipment" & vbTab & vbTab & "Half Day" & vbTab & "Full Day")
+        'lstRentalRates.Items.Add("1. Rug Cleaner" & vbTab & vbTab & "$16.00" & vbTab & "$24.00")
+        'lstRentalRates.Items.Add("2. Lawn Mover" & vbTab & vbTab & "$12.00" & vbTab & "$18.00")
+        'lstRentalRates.Items.Add("3. Paint Sprayer" & vbTab & vbTab & "$20.00" & vbTab & "$30.00")
+        DataGridView1.Rows.Add("1. Rug Cleaner", "$16.00", "$24.00")
+        DataGridView1.Rows.Add("2. Lawn Mover", "$12.00", "$18.00")
+        DataGridView1.Rows.Add("3. Rug Cleaner", "$20.00", "$30.00")
+
+    End Sub
+
+    Private Sub btnDisplayBill_Click(sender As Object, e As EventArgs) Handles btnDisplayBill.Click
+
+        If (txtItem.Text = "1" Or txtItem.Text = "2" Or txtItem.Text = "3") And (txtDuration.Text.ToUpper() = "H" Or txtDuration.Text.ToUpper() = "F") Then
+            Const deposit = 30
+            Dim item, duration As String
+            Dim itemCost As Double
+
+            item = txtItem.Text
+            duration = txtDuration.Text.ToUpper()
+
+            Select Case item
+                Case "1"
+                    item = "Rug Cleaner"
+                    itemCost = 16
+                Case "2"
+                    item = "Lawn Mover"
+                    itemCost = 12
+                Case "3"
+                    item = "Paint Sprayer"
+                    itemCost = 20
+
+            End Select
+
+            If duration = "F" Then
+                itemCost = itemCost + (itemCost * 0.5)
+                duration = "Full Day"
+            Else
+                duration = "Half Day"
+            End If
+
+            'lstBill.Items.Add("Receipt for your Equipment")
+            'lstBill.Items.Add("")
+            'lstBill.Items.Add(item & ":" & itemCost.ToString("C2") & " " & duration)
+            'lstBill.Items.Add("Deposit: " & deposit.ToString("C2"))
+            'lstBill.Items.Add("")
+            'lstBill.Items.Add("Total: " & (itemCost + deposit).ToString("C2"))
+            DataGridView2.Rows.Add(item, duration, itemCost.ToString("C2"), deposit.ToString("C2"), (itemCost + deposit).ToString("C2"))
+
+
+
+        Else
+            MessageBox.Show("Please correct your input", "Invalid Input", MessageBoxButtons.OK)
+        End If
+    End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentClick
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
