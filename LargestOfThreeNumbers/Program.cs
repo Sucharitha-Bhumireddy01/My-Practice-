a9ee7a1f18db8aab676dfa63bacb999c2de9abeb
Sucharitha-Bhumireddy01 <165252519+Sucharitha-Bhumireddy01@users.noreply.github.com>
2026-06-08 13:09:39 +0530

    Add files via upload

---
+++ LargestOfThreeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿//Write a C# program to find out largest number among three numbers, using "if".
+//Eg:
+
+//num1 = 60
+
+//num2 = 45
+
+//num3 = 123
+
+//Output: 123
+
+class LargestNumber
+{
+    static void Main()
+    {
+        int num1 = 60, num2 = 45, num3 = 123;
+        if (num1 > num2 && num1 > num3)
+            System.Console.WriteLine(num1);
+        else if (num2 > num1 && num2 > num3)
+            System.Console.WriteLine(num2);
+        else 
+            System.Console.WriteLine(num3);
+        System.Console.ReadKey();
+
+
+    }
+}
